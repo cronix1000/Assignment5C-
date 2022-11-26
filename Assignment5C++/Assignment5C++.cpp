3a6e32a11f6cae5a63cfdc8b762705d9a6dc3646
Idan Gomberg <crackenyogs10@gmail.com>
2022-11-26 00:05:16 -0500

    Add project files.

---
+++ Assignment5C++/Assignment5C++.cpp
@@ -0,0 +1,10 @@
+// Assignment5C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "main.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
