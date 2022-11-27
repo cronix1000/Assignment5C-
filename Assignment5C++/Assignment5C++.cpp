1be5cf28a54172b096dff2823388f63db84336a4
Idan Gomberg <crackenyogs10@gmail.com>
2022-11-26 22:05:45 -0500

    setup commands and item class

--- Assignment5C++/Assignment5C++.cpp
+++ Assignment5C++/Assignment5C++.cpp
@@ -4,7 +4,49 @@
 #include <iostream>
 #include "main.h"
 
+using namespace std;
+
+struct Item {
+	string name;
+	int id;
+};
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	header h;
+
+	Item items[10];
+	int itemcount = 0;
+	int command = 0;
+	while (command != 4) {
+		command = h.Commands();
+		if (command == 1) {
+			cout << "You walk down the path" << endl;
+		}
+		else if (command == 2) {
+			for (int i = 0; i < itemcount; i++) {
+				cout << items[i].name << endl;
+			}
+		}
+		else if (command == 3) {
+			cout << "Which item would you like to drop?" << endl;
+			string itemname;
+			cin >> itemname;
+			for (int i = 0; i < itemcount; i++) {
+				if (items[i].name == itemname) {
+					for (int j = i; j < itemcount; j++) {
+						items[j] = items[j + 1];
+					}
+					itemcount--;
+				}
+			}
+		}
+		else if (command == 4) {
+			cout << "You exit the program" << endl;
+		}
+		else {
+			cout << "Invalid Command" << endl;
+		}
+	}
 }
--- Assignment5C++/main.h
+++ Assignment5C++/main.h
@@ -1,1 +1,35 @@
-#pragma once
+namespace std
+{
+	class header
+	{
+	public:
+		int Commands() {
+			cout << "COMMAND MENU " << endl;
+			cout << "Walk - Walk down the path" << endl;
+			cout << "Show - Show all items" << endl;
+			cout << "Drop - Drop an Item" << endl;
+			cout << "Exit - Exit Program" << endl;
+			
+			string command;
+			cin >> command;
+			if (command == "Walk") {
+				return 1;
+			}
+			else if (command == "Show") {
+				return 2;
+			}
+			else if (command == "Drop") {
+				return 3;
+			}
+			else if (command == "Exit") {
+				return 4;
+			}
+			else {
+				cout << "Invalid Command" << endl;
+				return 0;
+			}
+			
+		}
+
+	};
+}
